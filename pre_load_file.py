a324a1eea19b8badbb85b4412749085bc0fc8071
Eddixon <eddx.castillo@gmail.com>
2024-11-16 15:46:07 +0100

    correlation matrixes

--- pre_load_file.py
+++ pre_load_file.py
@@ -19,32 +19,61 @@ def load_files():
     ds = ".\\DS\\Friday-WorkingHours-Afternoon-PortScan.pcap_ISCX.csv" 
     df_mon_08 = pd.read_csv(ds, encoding="utf-8")
 
+    
+    df_mon_01= df_mon_01.reset_index(drop=True)
+    df_mon_02= df_mon_02.reset_index(drop=True)
+    df_mon_03= df_mon_03.reset_index(drop=True)
+    df_mon_04= df_mon_04.reset_index(drop=True)
+    df_mon_05= df_mon_05.reset_index(drop=True)
+    df_mon_06= df_mon_06.reset_index(drop=True)
+    df_mon_07= df_mon_07.reset_index(drop=True)
+    df_mon_08= df_mon_08.reset_index(drop=True)
+    
+
     dfs = [df_mon_01, df_mon_02, df_mon_03,df_mon_04,df_mon_05,df_mon_06,df_mon_07,df_mon_08]
 
-    df_concat = pd.concat(dfs)
+    df_concat = pd.concat(dfs, ignore_index=True, axis=0)
     # Trimming the every column's name
     df_concat=df_concat.rename(str.strip, axis='columns')
+    
     return df_concat
 
 def generate_files(df_concat: pd.DataFrame ):
-    path_root = ".\\DS\\"
+    path_root = ".\\DS\\out\\"
     df_concat.to_csv(path_root+"df_full.csv")
+    print((path_root+"df_full.csv"))
+
     df_10p = df_concat.sample(frac=0.1)
     df_10p.to_csv(path_root+"df_10p.csv")
+    print(path_root+"df_10p.csv")
+
     df_4_309p = df_concat.sample(frac=0.04309)
     df_4_309p.to_csv(path_root+"df_4_309p.csv")
+    print(path_root+"df_4_309p.csv")
+
     df_20p = df_concat.sample(frac=0.2)
     df_20p.to_csv(path_root+"df_20p.csv")
+    print(path_root+"df_20p.csv")
+
     df_33p = df_concat.sample(frac=0.33)
     df_33p.to_csv(path_root+"df_33p.csv")
+    print(path_root+"df_33p.csv")
+
     df_10000 = df_concat.sample(n=10000)
     df_10000.to_csv(path_root+"df_10000.csv")
+    print(path_root+"df_10000.csv")
+
     df_50000 = df_concat.sample(n=50000)
     df_50000.to_csv(path_root+"df_50000.csv")
+    print(path_root+"df_50000.csv")
+    
     df_100000 = df_concat.sample(n=100000)
     df_100000.to_csv(path_root+"df_100000.csv")
+    print(path_root+"df_100000.csv")
+
     df_575000 = df_concat.sample(n=575000)
     df_575000.to_csv(path_root+"df_575000.csv")    
+    print(path_root+"df_575000.csv")    
 
 
 
